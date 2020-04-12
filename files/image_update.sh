65993c20dbdb9f131408faf4b6df5116aa70c164
tr <tr@brief.li>
2020-04-12 19:15:58 +0200

    Bash quoting

--- files/image_update.sh
+++ files/image_update.sh
@@ -5,9 +5,9 @@
 # Returns 2 if there is no change.
 # Returns 3 if something when wrong.
 #
-CONTAINER_IMAGE=$1
+CONTAINER_IMAGE="$1"
 
-if [ -z "${CONTAINER_IMAGE}" ];then
+if [ -z "${CONTAINER_IMAGE}" ]; then
   echo "Error no image passed"
   exit 1
 fi
@@ -17,14 +17,14 @@ if [[ ! "${CONTAINER_IMAGE}" =~ : ]]; then
   CONTAINER_IMAGE="${CONTAINER_IMAGE}:latest"
 fi
 
-BEFORE=$(podman inspect --type image --format='{{.Id}}' ${CONTAINER_IMAGE} 2>/dev/null)
-podman pull ${CONTAINER_IMAGE}
-AFTER=$(podman inspect --type image --format='{{.Id}}' ${CONTAINER_IMAGE} 2>/dev/null)
+BEFORE="$(podman inspect --type image --format='{{.Id}}' "${CONTAINER_IMAGE}" 2>/dev/null)"
+podman pull "${CONTAINER_IMAGE}"
+AFTER="$(podman inspect --type image --format='{{.Id}}' "${CONTAINER_IMAGE}" 2>/dev/null)"
 
-if [[ -z $AFTER ]]; then
+if [ -z "$AFTER" ]; then
   echo "Container image ${CONTAINER_IMAGE} failed to pull!"
   exit 3
-elif [[ $BEFORE == $AFTER ]]; then
+elif [ "$BEFORE" == "$AFTER" ]; then
   echo "No updates to ${CONTAINER_IMAGE} available. Currently on ${AFTER}."
   exit 2
 else
