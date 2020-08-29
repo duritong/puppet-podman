dc5f5a023809b184b0985148ad2cba9c65b8f0fc
tr <tr@immerda.ch>
2020-08-29 17:46:10 +0200

    Add some basic spec tests

--- spec/classes/podman_spec.rb
+++ spec/classes/podman_spec.rb
@@ -8,6 +8,17 @@ describe 'podman' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it { is_expected.to contain_package('podman') }
+      it { is_expected.to contain_package('slirp4netns') }
+      it { is_expected.to contain_package('runc') }
+
+      it { is_expected.to contain_file('/var/lib/containers/users').with(
+        :ensure => 'directory',
+        :owner  => 'root',
+        :group  => 'root',
+        :mode   => '0711',
+      )}
     end
   end
 end
