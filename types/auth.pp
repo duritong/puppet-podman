1c0df3b744d364d1512153982ea7188683ee00ed
mh <mh@immerda.ch>
2021-01-09 22:14:17 +0100

    allow trocla subsitutions

--- types/auth.pp
+++ types/auth.pp
@@ -1,7 +1,7 @@
 type Podman::Auth = Hash[
   Pattern[/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])(:\d+)?$/],
   Struct[{
-      user => Pattern[/^[a-zA-Z0-9_\.@\-]+$/],
-      password => Pattern[/^[a-zA-Z0-9\|\+\.\*\%\_]+$/],
+      user     => Pattern[/^[a-zA-Z0-9_\.@\-]+$/],
+      password => Variant[Pattern[/^%%TROCLA_.*%%$/],Pattern[/^[a-zA-Z0-9\|\+\.\*\%\_]+$/]],
   }]
 ]
