1aaa0159237906904d07c388b40c436651ff5dba
mh <mh@immerda.ch>
2021-01-10 10:25:23 +0100

    linting

--- types/socketports.pp
+++ types/socketports.pp
@@ -1,8 +1,8 @@
 type Podman::Socketports = Hash[
   Stdlib::Port,
   Struct[{
-    dir                       => Optional[Stdlib::Unixpath],
-    mode                      => Optional[Stdlib::Filemode],
-    'security-opt-label-type' => Optional[String[1]]
+      dir                       => Optional[Stdlib::Unixpath],
+      mode                      => Optional[Stdlib::Filemode],
+      'security-opt-label-type' => Optional[String[1]]
   }],
 ]
