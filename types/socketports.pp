ba8cb32f37a5089fea6f368b440a29a06614b804
mh <mh@immerda.ch>
2021-01-10 10:24:51 +0100

    add missing file

---
+++ types/socketports.pp
@@ -0,0 +1,8 @@
+type Podman::Socketports = Hash[
+  Stdlib::Port,
+  Struct[{
+    dir                       => Optional[Stdlib::Unixpath],
+    mode                      => Optional[Stdlib::Filemode],
+    'security-opt-label-type' => Optional[String[1]]
+  }],
+]
