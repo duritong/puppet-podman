bc21dba55047aa36c892bf2538058004874f3d63
mh <mh@immerda.ch>
2023-12-03 13:43:49 +0100

    correct naming of the variable

--- types/cronjobs.pp
+++ types/cronjobs.pp
@@ -1,11 +1,11 @@
 type Podman::Cronjobs = Hash[
   Pattern[/^[a-zA-Z0-9_\-]+$/],
   Struct[{
-      ensure              => Optional[Enum['present','absent']],
-      container_name      => Optional[String[1]],
-      cmd                 => String[1],
-      on_calendar         => Optional[String],
-      randomize_delay_sec => Optional[String],
-      trigger_restart     => Optional[Boolean],
+      ensure               => Optional[Enum['present','absent']],
+      container_name       => Optional[String[1]],
+      cmd                  => String[1],
+      on_calendar          => Optional[String],
+      randomized_delay_sec => Optional[String],
+      trigger_restart      => Optional[Boolean],
   }]
 ]
